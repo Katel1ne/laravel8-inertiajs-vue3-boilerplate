fc0cf47266e8f6ef0fffbc40284b232f7fd5259e
Katel1ine <d.v3nom@gmail.com>
2021-09-22 12:00:24 +0300

    Added missing resources

---
+++ resources/js/Pages/Test2.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <inertia-link :href="route('test1')">test1</inertia-link>
+    </div>
+</template>
+<script>
+import Default from './Layout/Default'
+
+export default {
+    layout: Default
+}
+</script>
